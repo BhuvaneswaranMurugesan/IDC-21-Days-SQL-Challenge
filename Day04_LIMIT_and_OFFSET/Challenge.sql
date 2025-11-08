3400040c795d56e95eb4ad7c4b3ca2eb9e84b7eb
BhuvaneswaranMurugesan <bhuvaneswaran.kanini@gmail.com>
2025-11-08 23:30:36 +0530

    Updated Day 4 Challenge

--- Day04_LIMIT_and_OFFSET/Challenge.sql
+++ Day04_LIMIT_and_OFFSET/Challenge.sql
@@ -1,3 +1,12 @@
 /*Question: Find the 3rd to 7th highest patient satisfaction scores from the patients table,
 showing patient_id, name, service, and satisfaction. Display only these 5 records.
 */
+SELECT
+	patient_id,
+	name,
+	service,
+	satisfaction
+FROM dbo.patients
+ORDER BY satisfaction DESC
+OFFSET 2 ROWS
+FETCH NEXT 5 ROWS ONLY;
